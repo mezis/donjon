77f3d92b0099b67c5c63a7f398d41d1dd4841d27
Julien Letessier <julien.letessier@gmail.com>
2014-06-23 14:30:25 +0200

    Adds EncryptedFile spec

---
+++ spec/donjon/encrypted_file_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+require 'donjon/encrypted_file'
+require 'donjon/user'
+require 'spec/support/repos'
+require 'spec/support/keys'
+
+describe Donjon::EncryptedFile do
+  let_repo :repo
+
+  let(:actor) {
+    Donjon::User.new(key: random_key, name: 'alice', repo: repo)
+  }
+
+  let(:other_user) {
+    Donjon::User.new(key: random_key, name: 'bob', repo: repo)
+  }
+
+  let(:options) {{
+    path: 'foo', actor: actor
+  }}
+
+  subject { described_class.new(**options) }
+
+  describe '#initialize' do
+    it 'passes with valid options' do
+      expect { subject }.not_to raise_error
+    end
+
+    it 'requires :path' do
+      options.delete :path
+      expect { subject }.to raise_error
+    end
+
+    it 'requires :actor' do
+      options.delete :actor
+      expect { subject }.to raise_error
+    end
+  end
+
+  describe '#write' do
+    before { actor.save } 
+
+    it 'passes' do
+      expect { subject.write('foo') }.not_to raise_error
+    end
+
+    it 'passes with large data' do
+      expect { subject.write('foo' * 1024) }.not_to raise_error
+    end
+
+    it 'works twice' do
+      expect {
+        2.times { subject.write 'foo' }
+      }.not_to raise_error
+    end
+
+    # it 'creates a file for the actor'
+    # it 'creates a file for other users'
+    # it 'encrypts the file' do
+  end
+
+  describe '#read' do
+    before do
+      actor.save
+      other_user.save
+
+      described_class.
+        new(actor: actor, path: options[:path]).
+        write('hello, world!')
+    end
+
+
+    it 'returns decrypted contents' do
+      expect(subject.read).to eq('hello, world!')
+    end
+
+    it 'works for other users' do
+      data = described_class.
+        new(actor: other_user, path: options[:path]).
+        read
+      expect(data).to eq('hello, world!')
+    end
+  end
+end
+
+
