00db07595d9e941c1ea88225baecbe478a986c44
Julien Letessier <julien.letessier@gmail.com>
2014-08-07 15:18:06 +0100

    Travis issue seems fixed?

--- lib/donjon/encrypted_file.rb
+++ lib/donjon/encrypted_file.rb
@@ -63,7 +63,7 @@ module Donjon
       payload = Gibberish::AES.new(decrypted_pw).decrypt(encrypted_data, binary: true)
       encoding = payload[0...32].strip
       data = payload[32...-PADDING].force_encoding(encoding)
-      $stderr.puts "decrypt encoding: #{encoding} (#{data})"
+      # $stderr.puts "decrypt encoding: #{encoding} (#{data})"
       data
     end
 
@@ -71,7 +71,7 @@ module Donjon
       encoding = data.encoding
       data = data.dup.force_encoding(Encoding::BINARY)
 
-      $stderr.puts "encrypt encoding: #{encoding} (#{data})"
+      # $stderr.puts "encrypt encoding: #{encoding} (#{data})"
       encoding_field = ("%-32s" % encoding).force_encoding(Encoding::BINARY)
       payload = encoding_field + data + OpenSSL::Random.random_bytes(PADDING)
       password = OpenSSL::Random.random_bytes(32)
